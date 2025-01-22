049e95f88d9bba4f6d44b87fbab7960ef9451820
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2025-01-22 10:41:13 +0200

    ft:implement the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,43 @@
-import Footer from '@/components/footer'
-import Navbar from '@/components/navbar'
-import React from 'react'
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { sections } from "@/lib/utils";
+import { AboutSection } from "@/components/aboutSection";
 
 const page = () => {
   return (
-    <div >
-      <Navbar/>
-       <h1 className="m-6">Welcome to the About page</h1>
-       <Footer/>
+    <div>
+      <Navbar />
+      <div className="max-w-4xl mx-auto p-8">
+        <h1 className="text-4xl font-bold mb-8">About Me.</h1>
+
+        {sections.map((section, index) => (
+          <AboutSection
+            key={index}
+            title={section.title}
+            content={section.content}
+            link={section.link}
+          />
+        ))}
+      </div>
+      <div className="flex gap-3 max-w-4xl mx-auto p-8">
+        <Link href="/projects" className="hover:underline">
+          Let's continue to Projects
+        </Link>
+        <Link
+          href="/projects"
+          className="animate-[moveArrow_1s_ease-in-out_infinite]"
+        >
+          <ArrowRight />
+        </Link>
+      </div>
+      <div className="max-w-4xl mx-auto p-8">
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
-
+export default page;
---
+++ lib/interfaces.ts
@@ -0,0 +1,10 @@
+export interface AboutSectionProps {
+    title: string;
+    content: string;
+    link?:{
+        url: string;
+        text: string;
+    }
+
+
+}
