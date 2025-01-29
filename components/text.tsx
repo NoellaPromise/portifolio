a97c854f6e77eb21a83b55819067483d75aec0fa
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2025-01-29 11:11:09 +0200

    Refactor home page text

--- components/text.tsx
+++ components/text.tsx
@@ -1,18 +1,30 @@
+import Link from "next/link";
 import React from "react";
 
 const text = () => {
   return (
     <div>
-      Your friendly neighborhood frontend developer, UX architect, and
-      JavaScript engineer. I spend my days (and often nights) painting the
-      Internet canvas with Projects and lines of code, turning zeroes and ones
-      into immersive, interactive experiences.  <br /> <br />
-      Bona fide photochromic Lens
-      enthusiast - sunlight or indoors, I have got it covered. I tread the path
-      of minimalism, finding beauty in simplicity and order. When Am not
-      crafting beautiful web experiences, you can find me reading Articles or
-      swaying to the rhythm of Pop Music & Jazz, losing myself in the
-      captivating flow of melodies. anyways you can Contact Me
+      I&apos;m a software engineer specializing in web and mobile development,
+      creating impactful{" "}
+      <Link
+        href="/projects"
+        className="font-bold text-black hover:underline underline-offset-2"
+      >
+        DIGITAL SOLUTIONS
+      </Link>{" "}
+      that solve real-world problems. I combine technical expertise with strong
+      collaborative skills to build innovative applications. Driven by
+      curiosity, I constantly explore emerging technologies to deliver better
+      user experiences. <br /> <br />
+      Beyond coding, I&apos;m a passionate videographer creating compelling {" "}
+      <Link
+        href="https://www.instagram.com/noella_promise/?hl=en"
+        className="font-bold text-black hover:underline underline-offset-2"
+      >
+        VISUAL STORIES
+      </Link>{" "}
+      . When I&apos;m not building applications or filming, you&apos;ll find me
+      unwinding to gospel music, finding inspiration in its peaceful melodies.
     </div>
   );
 };
